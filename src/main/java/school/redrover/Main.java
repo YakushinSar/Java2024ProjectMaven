7c82b0113983e89a2f1ff3233985e98428700704
YakushinSar <saratov482043162@gmail.com>
2024-10-26 21:53:15 +0400

    +

---
+++ src/main/java/school/redrover/Main.java
@@ -0,0 +1,9 @@
+package school.redrover;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("He" + StringUtils.leftPad("",5,"l") + "o world");
+        }
+    }
